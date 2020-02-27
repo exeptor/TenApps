5710774684f142c207eb35c85e7e80c1abc85226
svetlin.petrov.01@gmail.com <svetlin.petrov.01@gmail.com>
2020-02-27 23:38:16 +0000

    Seventh app add.

--- App_6_LOLCat_Factory/cat_service.py
+++ App_6_LOLCat_Factory/cat_service.py
@@ -4,12 +4,13 @@ import os
 
 def get_cat(folder, name):
     """
+    Download and save (under specified location) an object (picture) from a specified resource.
 
-
-    :param folder:
-    :param name:
-    :return:
+    :param folder: The folder where data should be stored.
+    :param name: The name of the object (picture).
+    :return: Data (picture) in a specified location.
     """
+
     url = 'http://consuming-python-services-api.azurewebsites.net/cats/random'
     data = get_data_from_url(url)
     save_image(folder, name, data)
@@ -17,17 +18,28 @@ def get_cat(folder, name):
 
 def get_data_from_url(url):
     """
+    Get raw data (binary) from a specified resource.
 
-
-    :param url:
-    :return:
+    :param url: The resource location (website url).
+    :return: Binary data (picture).
     """
+
     response = requests.get(url, stream=True)
 
     return response.raw
 
 
 def save_image(folder, name, data):
+    """
+    Save some data (e.g. picture) in a specified location.
+
+    :param folder: The folder where data should be stored.
+    :param name: Name of the object (e.g. picture).
+    :param data: Downloaded data (e.g. picture) stored in the memory.
+    :return: Object (e.g. picture) saved in a specified location.
+    """
+
     file_name = os.path.join(folder, name + '.jpg')
+
     with open(file_name, 'wb') as fout:
         shutil.copyfileobj(data, fout)
--- App_6_LOLCat_Factory/program_app_6.py
+++ App_6_LOLCat_Factory/program_app_6.py
@@ -9,8 +9,13 @@ def main():
 
     folder = get_or_create_output_folder()
     print('Found or created folder: {}'.format(folder))
+    print()
 
-    download_cats(folder)
+    count = int(input('Please, set a number of pictures to download: '))
+    if not isinstance(count, int):
+        count = int(input('Please, set a number of pictures to download: '))
+
+    download_cats(folder, count)
 
     display_cats(folder)
 
@@ -29,15 +34,16 @@ def print_header():
 
 def get_or_create_output_folder():
     """
+    Create a storing location (a folder) where the data will be downloaded and saved.
 
-
-    :return:
+    :return: The path to the folder location.
     """
 
     base_folder = os.path.dirname(__file__)
     folder = 'cat_factory'
     full_path = os.path.join(base_folder, folder)
 
+    # checking if the location (folder) with the same name already exists.
     if not os.path.exists(full_path) or not os.path.isdir(full_path):
         print('Creating new directory at {}'.format(full_path))
         os.mkdir(full_path)
@@ -45,16 +51,17 @@ def get_or_create_output_folder():
     return full_path
 
 
-def download_cats(folder):
+def download_cats(folder, count):
     """
+    Download (and show the download progress) a user specified number of data objects from a specified resource.
 
-
-    :param folder:
-    :return:
+    :param count: The number of objects to be downloaded.
+    :param folder: The folder where data has to be stored.
+    :return: Download data and shows the downloading progress.
     """
 
     print('Contacting server to download cats ...')
-    cats_count = 8
+    cats_count = count
     for i in range(1, cats_count + 1):
         name = 'lolcat_{}'.format(i)
         print('Downloading cat {}'.format(name))
@@ -65,14 +72,16 @@ def download_cats(folder):
 
 def display_cats(folder):
     """
+    Opens the folder with the downloaded data in explorer (for Windows) or the equivalent for MacOS and Linux.
 
-
-    :param folder:
-    :return:
+    :param folder: The folder where objects has to be stored.
+    :return: Triggers explorer with a specified location.
     """
+
     if platform.system() == 'Darwin':
         subprocess.call(['open', folder])
     elif platform.system() == 'Windows':
+        # todo: check why using 'explorer' instead of 'start' opens explorer with wrong (not 'folder') location.
         subprocess.call(['start', folder], shell=True)
     elif platform.system() == 'Linux':
         subprocess.call(['xdg-open', folder])
