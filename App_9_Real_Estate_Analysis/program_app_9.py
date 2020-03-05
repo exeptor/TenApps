a47fc3c44d1b32169d6645040cde8c6242cc6eee
svetlin.petrov.01@gmail.com <svetlin.petrov.01@gmail.com>
2020-03-05 17:52:42 +0000

    Update 9-th app.

--- App_9_Real_Estate_Analysis/program_app_9.py
+++ App_9_Real_Estate_Analysis/program_app_9.py
@@ -1,6 +1,11 @@
 import csv
 import os
 
+try:
+    import statistics
+except:
+    import Ten_Apps_Course.App_9_Real_Estate_Analysis.statistics_for_python2 as statistics
+
 from Ten_Apps_Course.App_9_Real_Estate_Analysis.data_types import Purchase
 
 
@@ -50,15 +55,67 @@ def load_file(filename):
 
 
 def query_data(data):
+    """
+    Calculate max price, min price, average price and average data for 2-bedroom home
+    :param data: List of objects.
+    :return: Print messages containing calculated values described above.
+    """
     data.sort(key=lambda p: p.price)
 
+    # highest home price.
     high_purchase = data[-1]
     print('The most expensive house is ${:,} with {} bedrooms and {} bathrooms.'.format(
         high_purchase.price, high_purchase.beds, high_purchase.baths))
 
+    # lowest home price.
     low_purchase = data[0]
     print('The cheapest house is ${:,} with {} bedrooms and {} bathrooms.'.format(
         low_purchase.price, low_purchase.beds, low_purchase.baths))
+
+    # average home price.
+    prices = [
+        p.price
+        for p in data
+    ]
+    avg_price = statistics.mean(prices)
+    print('The average home price is ${:,}.'.format(round(avg_price)))
+
+    # average data for 2-bedrooms home.
+    two_bed_home = [
+        p
+        for p in data
+        if p.beds == 2
+    ]
+
+    two_bed_avg_price = statistics.mean((p.price for p in two_bed_home))
+    two_bed_avg_baths = statistics.mean((p.baths for p in two_bed_home))
+    two_bed_avg_sq_ft = statistics.mean((p.sq__ft for p in two_bed_home))
+    print('Average 2-bedrooms home is ${:,}, {} baths and {} sq ft.'
+          .format(int(two_bed_avg_price), round(two_bed_avg_baths, 1), round(two_bed_avg_sq_ft, 1)))
+
+#     two_bed_home = (
+#         p
+#         for p in data
+#         if announce(p, '2-bedrooms, found {}'.format(p.beds)) and p.beds == 2
+#     )
+#
+#     homes = []
+#     for h in two_bed_home:
+#         if len(homes) >= 5:
+#             break
+#         homes.append(h)
+#
+#     two_bed_avg_price = statistics.mean((announce(p.price, 'price') for p in homes))
+#     two_bed_avg_baths = statistics.mean((p.baths for p in homes))
+#     two_bed_avg_sq_ft = statistics.mean((p.sq__ft for p in homes))
+#
+#     print('Average 2-bedrooms home is ${:,}, {} baths and {} sq ft.'
+#           .format(int(two_bed_avg_price), round(two_bed_avg_baths, 1), round(two_bed_avg_sq_ft, 1)))
+#
+#
+# def announce(item, msg):
+#     print('Pulling item {} for {}'.format(item, msg))
+#     return item
 
 
 if __name__ == '__main__':
